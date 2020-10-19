fa4247a0adf2fa2335e50dcfb2b39ee2924cedaa
Thy Endeth of Thy Lassy <54771755+Endlassy@users.noreply.github.com>
2020-10-19 02:00:11 +0200

    added the main goddam file
    no ones gonna clone it if there was only the readme.....
    
    unless ?

---
+++ panty.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from colorama import Fore, Style, Back
+
+a = f"""
+              {Fore.CYAN}P{Fore.WHITE}A{Fore.CYAN}N{Fore.WHITE}T{Fore.CYAN}S{Fore.WHITE}U{Fore.CYAN}!{Style.RESET_ALL}
+
+  {Back.CYAN}{Fore.CYAN}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀{Style.RESET_ALL}
+  {Back.WHITE}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀{Style.RESET_ALL}
+  {Back.CYAN}{Fore.CYAN}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀{Style.RESET_ALL}
+      {Back.WHITE}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀{Style.RESET_ALL}
+           {Back.CYAN}{Fore.CYAN}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀{Style.RESET_ALL}   
+              {Back.CYAN}{Fore.BLUE}⠀⠀⠀⠀⠀⠀{Style.RESET_ALL}   
+"""
+
+print(a)
